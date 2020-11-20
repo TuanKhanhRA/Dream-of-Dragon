d605a0396bb84c20d05c76c1f587194e0d5e0b4d
TuanKhanhRA <69688469+TuanKhanhRA@users.noreply.github.com>
2020-11-20 12:47:52 +0700

    Ui update

--- Dream-of-Dragon/Dream-of-Dragon/Main.cpp
+++ Dream-of-Dragon/Dream-of-Dragon/Main.cpp
@@ -1,10 +1,12 @@
 ﻿#include <Windows.h>
 #include <windowsx.h>
 #include "resource.h"
+#include "winres.h"
 
 //Chức năng giao tiếp với Windows, toàn bộ thông diệp đều được gửi qua hàm này
 //Hàm này đã được khai báo phía dưới
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK AboutBox(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
 //the entry point
@@ -25,8 +27,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.lpszClassName = L"WindowClass1";
+	//Load icon
 	wc.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 	wc.hIconSm = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+	//Load menu 
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	
 
 	RegisterClassEx(&wc);
 
@@ -48,11 +54,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	return msg.wParam;
 }
 
-// this is the main message handler for the program
+//Thủ tục để Vận hành một cửa sổ
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 	// sort through and find what code to run for the message given
 	switch (message){
 		// this message is read when the window is closed
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_EXIT:
+			PostQuitMessage(0);
+			break;
+		case ID_HELP_ABOUT:
+			DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG1), hWnd, AboutBox);
+			break;
+		}
 	case WM_DESTROY:{
 		// close the application entirely
 		PostQuitMessage(0);
@@ -63,3 +79,25 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 	// Handle any messages the switch statement didn't
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
+
+BOOL CALLBACK AboutBox(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg)
+	{
+	case WM_INITDIALOG:
+		return 1;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			EndDialog(hWnd, IDOK);
+			break;
+		case IDCANCEL:
+			EndDialog(hWnd, IDCANCEL);
+			break;
+		}
+		break;
+	default:
+		return 0;
+	}
+	return 1;
+	}
--- Dream-of-Dragon/Dream-of-Dragon/resource.h
+++ Dream-of-Dragon/Dream-of-Dragon/resource.h
@@ -3,13 +3,21 @@
 // Used by Resource.rc
 //
 #define IDI_ICON1                       103
+#define IDR_MENU1                       104
+#define IDD_DIALOG1                     105
+#define ID_FILE                         40001
+#define ID_FILE_EXIT                    40002
+#define ID_FILE_NEW                     40003
+#define ID_FILE_EXIT40004               40004
+#define ID_EDIT                         40005
+#define ID_HELP_ABOUT                   40006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40007
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
